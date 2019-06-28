12566bd2a76cc6696d8569d1f1c28a22e3f227f9
Darkglade <30379754+Darkglade@users.noreply.github.com>
2019-06-28 16:06:36 -0400

    Added Lucifer and buffed Amaterasu

---
+++ src/main/java/theWildCard/powers/PersonaLuciferPower.java
@@ -0,0 +1,53 @@
+package theWildCard.powers;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+import theWildCard.DefaultMod;
+import theWildCard.util.TextureLoader;
+
+import static theWildCard.DefaultMod.makePowerPath;
+
+
+public class PersonaLuciferPower extends AbstractPower {
+    public AbstractCreature source;
+
+    public static final String POWER_ID = DefaultMod.makeID("PersonaLuciferPower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("placeholder_power84.png"));
+    private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("placeholder_power32.png"));
+
+    public PersonaLuciferPower(final AbstractCreature owner, final AbstractCreature source) {
+        name = NAME;
+        ID = POWER_ID;
+
+        this.owner = owner;
+        this.source = source;
+
+        type = PowerType.BUFF;
+        isTurnBased = false;
+
+        //loads textures
+        this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        description = DESCRIPTIONS[0];
+    }
+
+    @Override
+    public void onApplyPower(AbstractPower power, AbstractCreature target, AbstractCreature source) {
+        if (power instanceof StrengthPower && power.amount > 0) {
+            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(this.owner, null,
+                    new StrengthPower(this.owner, power.amount), power.amount));
+        }
+    }
+}
