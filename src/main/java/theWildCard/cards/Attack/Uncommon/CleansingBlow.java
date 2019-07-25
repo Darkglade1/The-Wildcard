4bf8b93b13f3340c5d0d0981f6e185ef2021b29f
Darkglade <30379754+Darkglade@users.noreply.github.com>
2019-07-25 11:53:10 -0400

    Buffed cleansing strike and made deck a keyword to avoid any ambiguity

--- src/main/java/theWildCard/cards/Attack/Uncommon/CleansingBlow.java
+++ src/main/java/theWildCard/cards/Attack/Uncommon/CleansingBlow.java
@@ -3,6 +3,7 @@ package theWildCard.cards.Attack.Uncommon;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.unique.RemoveDebuffsAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -11,6 +12,9 @@ import theWildCard.WildcardMod;
 import theWildCard.cards.AbstractDefaultCard;
 import theWildCard.characters.WildcardCharacter;
 
+import java.util.ArrayList;
+
+import static com.megacrit.cardcrawl.core.CardCrawlGame.languagePack;
 import static theWildCard.WildcardMod.makeCardPath;
 
 public class CleansingBlow extends AbstractDefaultCard {
@@ -23,10 +27,9 @@ public class CleansingBlow extends AbstractDefaultCard {
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = WildcardCharacter.Enums.COLOR_BLUE;
 
-    private static final int COST = 2;
+    private static final int COST = 1;
 
-    private static final int DAMAGE = 7;
-    private static final int UPGRADE_PLUS_DMG = 5;
+    private static final int DAMAGE = 5;
 
     public CleansingBlow() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
@@ -39,13 +42,44 @@ public class CleansingBlow extends AbstractDefaultCard {
         AbstractDungeon.actionManager.addToBottom(
                 new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HEAVY));
         AbstractDungeon.actionManager.addToBottom(new RemoveDebuffsAction(AbstractDungeon.player));
+        ArrayList<AbstractCard> exhaustList = new ArrayList<>();
+        for (AbstractCard card : p.hand.group) {
+            if (card.type == CardType.CURSE || card.type == CardType.STATUS) {
+                exhaustList.add(card);
+            }
+        }
+        for (AbstractCard card : exhaustList) {
+            p.hand.moveToExhaustPile(card);
+        }
+        exhaustList.clear();
+
+        for (AbstractCard card : p.drawPile.group) {
+            if (card.type == CardType.CURSE || card.type == CardType.STATUS) {
+                exhaustList.add(card);
+            }
+        }
+        for (AbstractCard card : exhaustList) {
+            p.drawPile.moveToExhaustPile(card);
+        }
+        exhaustList.clear();
+
+        for (AbstractCard card : p.discardPile.group) {
+            if (card.type == CardType.CURSE || card.type == CardType.STATUS) {
+                exhaustList.add(card);
+            }
+        }
+        for (AbstractCard card : exhaustList) {
+            p.discardPile.moveToExhaustPile(card);
+        }
+        exhaustList.clear();
     }
 
     @Override
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeDamage(UPGRADE_PLUS_DMG);
+            this.exhaust = false;
+            rawDescription = languagePack.getCardStrings(cardID).UPGRADE_DESCRIPTION;
             initializeDescription();
         }
     }
